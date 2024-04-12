8613013fb149cdb180d2bec7e63cd3ac9689db5b
Nathan Van Bignoot <nathan.vanbignoot@icloud.com>
2024-04-12 13:58:57 +0200

    fix : index.html UI

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,42 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographer(param) {
+  try {
+    const urlParams = new URLSearchParams(param);
+    const urlId = urlParams.get("id");
+    console.log("Extracted ID:", urlId);
+
+    const response = await fetch(
+        "http://localhost:63342/Front-End-Fisheye/data/photographers.json",
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const photographers = data.photographers;
+
+    const photographer = photographers.find(
+        (photographer) => photographer.id === parseInt(urlId),
+    );
+    console.log("Found Photographer:", photographer);
+    return photographer;
+  } catch (e) {
+    console.error('There was a problem with fetching the photographer:', e);
+  }
+}
+
+async function displayPhotographer (data) {
+  const dataSection = document.querySelector(".data_section");
+  const portrait = `assets/photographers/${data.portrait}`
+  dataSection.innerHTML = `
+      <h2>${data.name}</h2>
+      <p>${data.city}, ${data.country}</p>
+      <p>${data.tagline}</p>
+      <img src="${portrait}" alt="">
+  `;
+}
+
+async function init() {
+  const photographer = await getPhotographer(window.location.search);
+  displayPhotographer(photographer);
+}
+
+init()
---
+++ scripts/templates/index.js
@@ -0,0 +1,36 @@
+function indexTemplate(data) {
+    const {id, name, portrait, tagline, city, price} = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const a = document.createElement("a");
+        a.setAttribute(
+            "href",
+            "http://localhost:63342/Front-End-Fisheye/photographer.html?id=" + id,
+        );
+        const article = document.createElement("article");
+        const div = document.createElement("div")
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        const h4 = document.createElement("h4");
+        const p = document.createElement("p")
+        h2.textContent = name;
+        h3.textContent = city;
+        h4.textContent = tagline;
+
+        p.textContent = price + "€/Jour"
+        a.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(div);
+        div.appendChild(h3);
+        div.appendChild(h4);
+        div.appendChild(p)
+        return a;
+    }
+
+    return {name, picture, getUserCardDOM};
+}
